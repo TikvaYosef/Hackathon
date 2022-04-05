6bfb8c3c3f2d27d6aa290f5fbee8a772f2e0f22a
JacobShiber <yaakovshiber@gmail.com>
2022-04-06 02:38:07 +0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <FirebaseProvider>
         <Workspace />
-        <LoginPage />
-        <Footer />
+        {/* <LoginPage />
+        <Footer /> */}
     </FirebaseProvider>
   );
 }
