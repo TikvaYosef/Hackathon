22805da316eefe7520023b262090a3bce01a7e93
Amir-Mangisto <mangisto.man@gmail>
2022-04-05 18:49:43 +0300

    adding comapnies model and routes

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ const app = express();
 const port = process.env.PORT;
 
 const workflowRouter = require('./routes/workflow-process-route');
-
+const companiesRouter = require('./routes/companies-process-route');
 app.use('/workflow', workflowRouter);
+app.use('/company',companiesRouter);
+
 
 app.get('/', (req, res) => {
     res.status(200).send('server is online');
