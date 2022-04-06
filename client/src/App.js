01fce88df210274ab729c7756a5721ba1924ebc7
TikvaYosef <tikvayosef13@gmail.com>
2022-04-06 10:10:09 +0300

    h

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Footer } from './components/layout/Footer/Footer';
+import { Footer } from './components/layout/Footer/Footer.jsx';
 import { LoginPage } from './components/pages/LoginPage/LoginPage';
 import FirebaseProvider from './context/FirebaseContext';
+import Companys_Page from './components/companies-page-component/Companies_page';
 
 function App() {
   return (
-    <FirebaseProvider>
+    <>
+      <FirebaseProvider>
         <LoginPage />
-        <Footer />
-    </FirebaseProvider>
+      </FirebaseProvider>
+      <Footer />
+      <Companys_Page/>
+    </>
   );
 }
 
---
+++ client/src/components/companies-page-component/Companies_page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default  function Companys_Page(){
+
+    return(
+    <>
+     <TextField  label="Outlined" variant="outlined" />
+     <Button>CLICK</Button>
+     
+    </>
+
+    )
+}
